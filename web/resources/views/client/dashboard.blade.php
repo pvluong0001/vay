c8fd6ceeb18b85184c5654170a692eb0b6d720b1
luongpv <pvluong0001@gmail.com>
2021-01-02 18:10:11 +0700

    optimize page load point

--- web/resources/views/client/dashboard.blade.php
+++ web/resources/views/client/dashboard.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lit</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="preload" href="{{ asset('css/app.css') }}" as="style">
 </head>
 <body>
 <!-- top info -->
